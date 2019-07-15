0679b9ee9902b15efd504a0ac3b4c6af86cabb65
Räganr O'ock <38219333+UgoDumontot@users.noreply.github.com>
2019-07-16 00:40:05 +0200

    desable all

--- run.py
+++ run.py
@@ -8,14 +8,15 @@ from credentials import nickname, oauth
 client = IRC(nickname, oauth)
 
 
-with open(path.join("samples", "samples1.txt"), 'r', encoding='utf-8') as file:
-    sample = file.read()
+# with open(path.join("samples", "samples1.txt"), 'r', encoding='utf-8') as file:
+#     sample = file.read()
+#
+# i = 0
+# for shit in sample.split('\n'):
+#     i += 1
+#     print(json.dumps(client.parse(shit).__dict__, indent=2))
+#     if i == 30:
+#         break
 
-i = 0
-for shit in sample.split('\n'):
-    i += 1
-    print(json.dumps(client.parse(shit).__dict__, indent=2))
-    if i == 30:
-        break
 while True:
     time.sleep(0.1)
