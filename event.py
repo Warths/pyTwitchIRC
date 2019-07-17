4d98f1ab7fec1c34f86417e88abbe4510d947f1d
Warths <pro.warths@gmail.com>
2019-07-17 20:27:08 +0200

    Event class reworked CurrentEvent subclass created

--- event.py
+++ event.py
@@ -3,7 +3,7 @@ import json
 
 class Event:
 
-    def __init__(self, raw: str, content=None, type=None, tags=None, author=None, channel=None):
+    def __init__(self, raw=None, content=None, type=None, tags=None, author=None, channel=None):
         """
 
         :param raw: decoded IRC event
@@ -28,3 +28,18 @@ class Event:
         \tauthor : {},
         \tchannel : {},
         \tcontent : {}\r\n""".format(self.raw, self.type, self.tags, self.author, self.channel, self.content))
+
+
+class CurrentEvent(Event):
+
+    def __init__(self, raw=None, content=None, type=None, tags=None, author=None, channel=None):
+        Event.__init__(self, raw, content, type, tags, author, channel)
+
+    def update(self, event):
+        self.type = event.type
+        self.tags = event.tags
+        self.author = event.author
+        self.channel = event.channel
+        self.content = event.content
+
+
